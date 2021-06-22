a6b21dd7c63db7b9ef89abc46da4acb2264337cd
Eyvazov <contact@azadeyvazov.com>
2021-06-22 16:37:50 +0400

    Quiz migration-u, model, factory-i və seed əlavə edildi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::insert([
-            'name' => 'Azad Eyvazov',
-            'email' => 'azad.eyvazov99@gmail.com',
-            'email_verified_at' => now(),
-            'type' => 'admin',
-            'password' => bcrypt('azad1234'),
-            'remember_token' => Str::random(10),
+        $this->call([
+            UserSeeder::class,
+            QuizSeeder::class,
         ]);
-        \App\Models\User::factory(5)->create();
     }
 }
