7c15023931b2ad342f4683448353bbb73836c979
Eyvazov <contact@azadeyvazov.com>
2021-06-22 15:48:25 +0400

    isAdmin Middleware-i əlavə edildi. Panel-ə user type-i admin olanalrdan başqa heç kim daxil ola bilməyəcək

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::insert([
+            'name' => 'Azad Eyvazov',
+            'email' => 'azad.eyvazov99@gmail.com',
+            'email_verified_at' => now(),
+            'type' => 'admin',
+            'password' => bcrypt('azad1234'),
+            'remember_token' => Str::random(10),
+        ]);
+        \App\Models\User::factory(5)->create();
     }
 }
